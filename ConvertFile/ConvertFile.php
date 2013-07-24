155241231447974eed053611426ef5001c151da7
Tommas Yuan <tommas1988@yahoo.cn>
2013-07-24 22:30:09 +0800

    add docblock to ConvertFile

--- ConvertFile/ConvertFile.php
+++ ConvertFile/ConvertFile.php
@@ -1,16 +1,48 @@
 <?php
+/**
+ * CharsetConvertor
+ * 
+ * @author Tommas Yuan
+ * @link   http://github.com/tommas1988/CharsetConvertor the source code repository
+ */
+
 namespace Tcc\ConvertFile;
 
 use SplFileInfo;
 use IteratorAggregate;
 use InvalidArgumentException;
 
+/**
+ * ConvertFile class
+ */
 class ConvertFile implements IteratorAggregate
 {
+    /**
+     * Input charset encoding
+     * @var string
+     */
     protected $inputCharset;
+
+    /**
+     * Output charset encoding
+     * @var string
+     */
     protected $outputCharset;
+
+    /**
+     * @var SplFileInfo
+     */
     protected $fileInfo;
     
+    /**
+     * Constructor
+     *
+     * @param  string|SplFileInfo $file
+     * @param  string $inputCharset
+     * @param  string $outputCharset
+     * @throws InvalidArgumentException If convert file is not string or SplFileInfo
+     * @throws InvalidArgumentException If convert file is not file or readable
+     */
     public function __construct($file, $inputCharset, $outputCharset)
     {
         if (is_string($file)) {
@@ -29,21 +61,36 @@ class ConvertFile implements IteratorAggregate
         $this->outputCharset = $outputCharset;
     }
 
+    /**
+     * Implements from AggregateIterator
+     *
+     * @return SplFileObject
+     */
     public function getIterator()
     {
         return $this->fileInfo->openFile();
     }
     
+    /**
+     * @return string
+     */
     public function getInputCharset()
     {
         return $this->inputCharset;
     }
     
+    /**
+     * @return string
+     */
     public function getOutputCharset()
     {
         return $this->outputCharset;
     }
 
+    /**
+     * @param  bool $withoutExtension Return filename with suffix or not
+     * @return string
+     */
     public function getFilename($withoutExtension = false)
     {
         $fileInfo = $this->fileInfo;
@@ -56,16 +103,25 @@ class ConvertFile implements IteratorAggregate
         return $fileInfo->getFilename();
     }
 
+    /**
+     * @return string The file path
+     */
     public function getPath()
     {
         return ConvertFileContainer::canonicalPath($this->fileInfo->getPath());
     }
 
+    /**
+     * @return string The file path and file name
+     */
     public function getPathname()
     {
         return ConvertFileContainer::canonicalPath($this->fileInfo->getPathname());
     }
 
+    /**
+     * @return string
+     */
     public function getExtension()
     {
         return ConvertFileContainer::canonicalExtension($this->fileInfo->getExtension());
